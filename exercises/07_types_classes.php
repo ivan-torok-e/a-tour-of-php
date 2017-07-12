233a05e7afd63a0ff08e909a4710869189be8fa2
Adam Luzsi <adamluzsi@gmail.com>
2017-07-12 07:23:56 +0200

    types and classes exercise

---
+++ exercises/07_types_classes.php
@@ -0,0 +1,23 @@
+<?php
+
+// In php I usually separate two main group when we talk about "values"/"objects"
+//
+// Data Types or primitives
+//    * String
+//    * Integer
+//    * Float (floating point numbers - also called double)
+//    * Boolean
+//    * Array
+//    * Object
+//    * NULL
+//
+// ---
+//
+// Your task here is to
+//    * create a new class by your self alone
+//      * you can use the previous exercises
+//    * create a public method with the name of #checkTypeOf
+//    * it takes one parameter
+//    * it will return the received value's type as a string
+//      * you must use the use the internet for this in order to find out how to get a values type
+//
